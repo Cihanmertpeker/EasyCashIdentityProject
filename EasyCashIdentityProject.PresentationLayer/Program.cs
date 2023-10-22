b242c83f6a39ea860928faf9ed3df5ca777075be
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2023-10-22 22:49:00 +0300

    1

--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,8 +1,12 @@
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,7 +17,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
